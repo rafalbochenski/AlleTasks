797420d4218fa8d10a9a46abf9df4a86bb38707d
Rafal Bochenski <rafalbochenski@MacBook-Rafal.local>
2020-11-10 23:20:07 +0100

    Tests for service 2&3

---
+++ Rafal.Bochenski.Service2.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Rafal.Bochenski.Service2.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
